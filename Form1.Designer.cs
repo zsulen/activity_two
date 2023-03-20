4fb42224924251c8dc52e9e9f7421ff550665d2d
zsulen <93764289+zsulen@users.noreply.github.com>
2023-03-19 22:14:18 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.wishList = new System.Windows.Forms.ListBox();
+            this.fillB = new System.Windows.Forms.Button();
+            this.sortB = new System.Windows.Forms.Button();
+            this.clearB = new System.Windows.Forms.Button();
+            this.countB = new System.Windows.Forms.Button();
+            this.closeB = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(50, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Wish List";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // wishList
+            // 
+            this.wishList.FormattingEnabled = true;
+            this.wishList.Location = new System.Drawing.Point(56, 112);
+            this.wishList.Name = "wishList";
+            this.wishList.Size = new System.Drawing.Size(228, 212);
+            this.wishList.TabIndex = 1;
+            this.wishList.SelectedIndexChanged += new System.EventHandler(this.wishList_SelectedIndexChanged);
+            // 
+            // fillB
+            // 
+            this.fillB.Location = new System.Drawing.Point(344, 134);
+            this.fillB.Name = "fillB";
+            this.fillB.Size = new System.Drawing.Size(75, 23);
+            this.fillB.TabIndex = 2;
+            this.fillB.Text = "Fill";
+            this.fillB.UseVisualStyleBackColor = true;
+            this.fillB.Click += new System.EventHandler(this.fillB_Click);
+            // 
+            // sortB
+            // 
+            this.sortB.Location = new System.Drawing.Point(344, 170);
+            this.sortB.Name = "sortB";
+            this.sortB.Size = new System.Drawing.Size(75, 23);
+            this.sortB.TabIndex = 3;
+            this.sortB.Text = "Sort";
+            this.sortB.UseVisualStyleBackColor = true;
+            this.sortB.Click += new System.EventHandler(this.sortB_Click);
+            // 
+            // clearB
+            // 
+            this.clearB.Location = new System.Drawing.Point(344, 208);
+            this.clearB.Name = "clearB";
+            this.clearB.Size = new System.Drawing.Size(75, 23);
+            this.clearB.TabIndex = 4;
+            this.clearB.Text = "Clear";
+            this.clearB.UseVisualStyleBackColor = true;
+            this.clearB.Click += new System.EventHandler(this.clearB_Click);
+            // 
+            // countB
+            // 
+            this.countB.Location = new System.Drawing.Point(344, 247);
+            this.countB.Name = "countB";
+            this.countB.Size = new System.Drawing.Size(75, 23);
+            this.countB.TabIndex = 5;
+            this.countB.Text = "Count";
+            this.countB.UseVisualStyleBackColor = true;
+            this.countB.Click += new System.EventHandler(this.countB_Click);
+            // 
+            // closeB
+            // 
+            this.closeB.Location = new System.Drawing.Point(344, 282);
+            this.closeB.Name = "closeB";
+            this.closeB.Size = new System.Drawing.Size(75, 23);
+            this.closeB.TabIndex = 6;
+            this.closeB.Text = "Close";
+            this.closeB.UseVisualStyleBackColor = true;
+            this.closeB.Click += new System.EventHandler(this.closeB_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 398);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Selection:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 442);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Count:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 391);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(171, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(113, 435);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(171, 20);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 529);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.closeB);
+            this.Controls.Add(this.countB);
+            this.Controls.Add(this.clearB);
+            this.Controls.Add(this.sortB);
+            this.Controls.Add(this.fillB);
+            this.Controls.Add(this.wishList);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox wishList;
+        private System.Windows.Forms.Button fillB;
+        private System.Windows.Forms.Button sortB;
+        private System.Windows.Forms.Button clearB;
+        private System.Windows.Forms.Button countB;
+        private System.Windows.Forms.Button closeB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wishList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = wishList.Text;
+        }
+
+        private void fillB_Click(object sender, EventArgs e)
+        {
+            wishList.Items.Add("Taylor Swift concert ticket");
+            wishList.Items.Add("Blackpink concert ticket");
+            wishList.Items.Add("iPhone XR");
+        }
+
+        private void sortB_Click(object sender, EventArgs e)
+        {
+            wishList.Sorted = true;
+        }
+
+        private void clearB_Click(object sender, EventArgs e)
+        {
+            wishList.Items.Clear();
+        }
+
+        private void countB_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = Convert.ToString(wishList.Items.Count);
+        }
+
+        private void closeB_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
